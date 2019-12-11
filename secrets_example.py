8d794a7f6890fad3479deefa58ae731f0e027e53
inesp <ines.panker@gmail.com>
2019-12-11 17:25:22 +0100

    Add an example of the secrets file

---
+++ secrets_example.py
@@ -0,0 +1,10 @@
+BITBUCKET_TOKEN = 'NREFD..........A%w'  # Personal Token created on BitBucket
+BITBUCKET_USERNAME = 'first_name.last_name'  # your Bitbucket username
+BITBUCKET_REPO_LINK = 'https://LOCALHOST:123456/rest/api/1.0/projects/PROJECT/repos/REPO'
+
+BITBUCKET_USER_NAME_TO_SLACK_USER_NAME = {
+
+}
+
+# Slack's webhook link (Incoming WebHooks), which you get Slack's App Directory at probably http://<your slack>/apps
+SLACK_WEBHOOK_URL= 'https://hooks.slack.com/services/ASDFGASDFG....'
